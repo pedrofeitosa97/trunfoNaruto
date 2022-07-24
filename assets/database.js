74477a7c2e02edd1846de8e814dbaa75edaf8fcb
bobjoe159 <pedroh1001@hotmail.com>
2022-07-24 11:06:09 -0300

    adicionados novos personagens

--- assets/database.js
+++ assets/database.js
@@ -75,7 +75,7 @@ const personagens = [
     nome: 'Sandaime Hokage (Sarutobi)',
     foto: './img/sandaime.png',
     aldeia: 'Folha',
-    ninjutsu: '95',
+    ninjutsu: '99',
     taijutsu: '75',
     genjutsu: '55',
     inteligencia: '90'
@@ -151,5 +151,50 @@ const personagens = [
     taijutsu: '80',
     genjutsu: '65',
     inteligencia: '80'
+  },
+  {
+    nome: 'Yahiko Pain',
+    foto: './img/pain.png',
+    aldeia: 'chuva',
+    ninjutsu: '85',
+    taijutsu: '80',
+    genjutsu: '65',
+    inteligencia: '85'
+  },
+  {
+    nome: 'Hyuuga Hinata',
+    foto: './img/hinata.png',
+    aldeia: 'folha',
+    ninjutsu: '75',
+    taijutsu: '80',
+    genjutsu: '65',
+    inteligencia: '75'
+  },
+  {
+    nome: 'Inuzuka Kiba',
+    foto: './img/kiba.png',
+    aldeia: 'folha',
+    ninjutsu: '70',
+    taijutsu: '85',
+    genjutsu: '65',
+    inteligencia: '65'
+  },
+  {
+    nome: 'Yamanaka Ino',
+    foto: './img/inoyamanaka.png',
+    aldeia: 'folha',
+    ninjutsu: '65',
+    taijutsu: '50',
+    genjutsu: '85',
+    inteligencia: '80'
+  },
+  {
+    nome: 'Kabuto Yakushi',
+    foto: './img/kabuto.png',
+    aldeia: 'folha',
+    ninjutsu: '85',
+    taijutsu: '40',
+    genjutsu: '70',
+    inteligencia: '90'
   }
 ]
